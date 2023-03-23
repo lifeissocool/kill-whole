d330cdb218e53dd42c2091cb2245529d090dc4a7
cuiym <cuiym@ziroom.com>
2023-03-23 17:18:18 +0800

    test out

--- src/main/java/com/functions/Function.java
+++ src/main/java/com/functions/Function.java
@@ -15,8 +15,14 @@ import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FixedDelayRetry;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
+import java.io.IOException;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Azure Functions with HTTP Trigger.
@@ -24,6 +30,13 @@ import java.util.Optional;
 public class Function {
     public static int count = 1;
 
+    public static  OkHttpClient okHttpClient=null;
+    static {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.retryOnConnectionFailure(false);
+        builder.connectionPool(new ConnectionPool(30, 30, TimeUnit.MINUTES));
+        okHttpClient = builder.build();
+    }
     /**
      * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
@@ -36,18 +49,20 @@ public class Function {
                 methods = {HttpMethod.GET, HttpMethod.POST},
                 authLevel = AuthorizationLevel.ANONYMOUS)
                 HttpRequestMessage<Optional<String>> request,
-            final ExecutionContext context) {
+            final ExecutionContext context) throws IOException {
         context.getLogger().info("Java HTTP trigger processed a request.");
 
         // Parse query parameter
         final String query = request.getQueryParameters().get("name");
         final String name = request.getBody().orElse(query);
 
+        Request req = new Request.Builder().url("https://google.com").get().build();
+        Response execute = okHttpClient.newCall(req).execute();
 
         if (name == null) {
             return request.createResponseBuilder(HttpStatus.BAD_REQUEST).body("Please pass a name on the query string or in the request body").build();
         } else {
-            return request.createResponseBuilder(HttpStatus.OK).body("Hello, " + name).build();
+            return request.createResponseBuilder(HttpStatus.OK).body(execute.body().string()).build();
         }
     }
 
